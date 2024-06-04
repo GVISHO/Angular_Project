a41837bcfd4e2814c3def1653b4d36151349f760
GVISHO <nodargvishiani@gmail.com>
2024-06-04 22:16:38 +0400

    Fix:Fixed brands page now user can switch simultaneously between brands

--- src/app/features/brand-products/brand-products.component.ts
+++ src/app/features/brand-products/brand-products.component.ts
@@ -1,26 +1,53 @@
-import { Component,inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductPreviewComponent } from '../../shared/ui/product-preview/product-preview.component';
 import { AsyncPipe } from '@angular/common';
-import { EMPTY, catchError } from 'rxjs';
-import { ActivatedRoute, Router } from '@angular/router';
+import { BehaviorSubject, EMPTY, catchError, filter, tap } from 'rxjs';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { ProductsService } from '../../shared/services';
+import { Products } from '../../shared/interfaces';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-brand-products',
   standalone: true,
-  imports: [ProductPreviewComponent,AsyncPipe],
+  imports: [ProductPreviewComponent, AsyncPipe],
   templateUrl: './brand-products.component.html',
-  styleUrl: './brand-products.component.css'
+  styleUrl: './brand-products.component.css',
 })
-export default class BrandProductsComponent   {
+export default class BrandProductsComponent {
   private readonly router = inject(Router);
   private readonly activatedRoute = inject(ActivatedRoute);
   private readonly productService = inject(ProductsService);
-  readonly products$ = this.productService.getProductsByBrand(this.activatedRoute.snapshot.params['brand']).pipe(
-    catchError(() => {
-      this.router.navigateByUrl('404');
-      return EMPTY;
-    }),
-  );
 
+  readonly #products$ = new BehaviorSubject<Products | null>(null);
+  readonly products$ = this.#products$.asObservable();
+
+  constructor() {
+    this.router.events
+      .pipe(
+        takeUntilDestroyed(),
+        filter((event) => event instanceof NavigationEnd),
+        tap(() => {
+          let snapshopt = this.activatedRoute.snapshot;
+
+          while (snapshopt.firstChild) {
+            snapshopt = snapshopt.firstChild;
+          }
+
+          this.productService
+            .getProductsByBrand(snapshopt.params['brand'])
+            .pipe(
+              tap((response) => {
+                this.#products$.next(response);
+              }),
+              catchError(() => {
+                this.router.navigateByUrl('404');
+                return EMPTY;
+              }),
+            )
+            .subscribe();
+        }),
+      )
+      .subscribe();
+  }
 }
