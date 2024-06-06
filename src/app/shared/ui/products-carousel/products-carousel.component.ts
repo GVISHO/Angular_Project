d3132c5282e4c319a6662d555c0d884767d076ea
GVISHO <nodargvishiani@gmail.com>
2024-06-06 17:27:50 +0400

    Style:Made carousel item ,rating stars and product price responsive

--- src/app/shared/ui/products-carousel/products-carousel.component.ts
+++ src/app/shared/ui/products-carousel/products-carousel.component.ts
@@ -3,12 +3,13 @@ import { Product } from '../../interfaces';
 import { ProductRatingStarsComponent } from '../product-rating-stars/product-rating-stars.component';
 import { ProductPriceComponent } from '../product-price/product-price.component';
 import { RouterLink } from '@angular/router';
+import { TextShorterPipe } from '../../pipes/text-shorter.pipe';
 
 
 @Component({
   selector: 'app-products-carousel',
   standalone: true,
-  imports: [ProductRatingStarsComponent,ProductPriceComponent,RouterLink],
+  imports: [ProductRatingStarsComponent,ProductPriceComponent,RouterLink,TextShorterPipe],
   templateUrl: './products-carousel.component.html',
   styleUrl: './products-carousel.component.css'
 })
